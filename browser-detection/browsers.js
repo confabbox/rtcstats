8eb9fd025e567bb047dc1f0002aebd6bf3d75b50
venkateshsundaram <contactvenkateshsundaram@gmail.com>
2022-03-20 11:29:07 +0530

    Initial commit

---
+++ browser-detection/browsers.js
@@ -0,0 +1,17 @@
+export const CHROME = 'chrome';
+
+export const OPERA = 'opera';
+
+export const FIREFOX = 'firefox';
+
+export const INTERNET_EXPLORER = 'iexplorer';
+
+export const SAFARI = 'safari';
+
+export const NWJS = 'nwjs';
+
+export const ELECTRON = 'electron';
+
+export const REACT_NATIVE = 'react-native';
+
+export const UNKNOWN = 'unknown';
